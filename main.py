17b31ae24a759081ada82a518c8904ff8507757e
ArtOfCode- <owen.jenkins2903@gmail.com>
2015-11-04 16:09:22 +0000

    fix NoneType iteration error

--- main.py
+++ main.py
@@ -81,22 +81,20 @@ def get_tags():
     while has_more and page <= 20:
         print("Retrieving page #{0} of tags...".format(page))
 
-        response, has_even_more, backoff = None
         try:
             response, has_even_more, backoff = apiManager.request("/tags", {'filter': tag_filter, 'pagesize': 100, 'page': page})
+            has_more = has_even_more
+
+            parse_tags_response(response)
+
+            if backoff > 0:
+                print("Back-off received. Waiting {0} seconds before trying again.".format(backoff))
+                time.sleep(backoff)
+
+            page += 1
         except APIException as ex:
             print("[API] [ERROR] Could not fetch tags: #{0} '{1}' - {2}".format(ex.id, ex.name, ex.message))
             continue
-
-        has_more = has_even_more
-
-        parse_tags_response(response)
-
-        if backoff > 0:
-            print("Back-off received. Waiting {0} seconds before trying again.".format(backoff))
-            time.sleep(backoff)
-
-        page += 1
 
     print("Finished processing all tags.")
 
@@ -110,17 +108,15 @@ def get_question(id):
 
     print("Fetching question #{0}...".format(id))
 
-    response, has_more, backoff = None
     try:
-        apiManager.request("/questions/" + str(id), {'filter': question_filter})
+        response, has_more, backoff = apiManager.request("/questions/" + str(id), {'filter': question_filter})
+        item = response["items"][0]
+        if "closed_date" not in item or ("closed_date" in item and item["closed_date"] is None):
+            return item["title"], item["body"], item["tags"], item["question_id"]
+        else:
+            return None
     except APIException as ex:
         print("[API] [ERROR] Could not fetch tags: #{0} '{1}' - {2}".format(ex.id, ex.name, ex.message))
-        return None
-
-    item = response["items"][0]
-    if "closed_date" not in item or ("closed_date" in item and item["closed_date"] is None):
-        return item["title"], item["body"], item["tags"], item["question_id"]
-    else:
         return None
 
 def get_questions(ids):
@@ -140,20 +136,18 @@ def get_questions(ids):
 
     print("Fetching questions #" + ", #".join(ids) + "...")
 
-    response, has_more, backoff = None
     try:
-        apiManager.request("/questions/{0}".format(id_list), {'filter': question_filter})
+        response, has_more, backoff = apiManager.request("/questions/{0}".format(id_list), {'filter': question_filter})
+        for item in response["items"]:
+            if "closed_date" not in item or ("closed_date" in item and item["closed_date"] is None):
+                questions.append({'title': item["title"], 'body': item["body"], 'tags': item["tags"], 'id': item["question_id"]})
+            else:
+                print("Question #" + str(item["question_id"]) + " is closed.")
+
+        return questions
     except APIException as ex:
         print("[API] [ERROR] Could not fetch tags: #{0} '{1}' - {2}".format(ex.id, ex.name, ex.message))
         return None
-
-    for item in response["items"]:
-        if "closed_date" not in item or ("closed_date" in item and item["closed_date"] is None):
-            questions.append({'title': item["title"], 'body': item["body"], 'tags': item["tags"], 'id': item["question_id"]})
-        else:
-            print("Question #" + str(item["question_id"]) + " is closed.")
-
-    return questions
 
 def get_all_questions():
     questions = []
@@ -164,24 +158,22 @@ def get_all_questions():
     while has_more and page <= 20:
         print("Fetching page #{0} of questions...".format(page))
 
-        response, has_even_more, backoff = None
         try:
-            apiManager.request("/questions", {'filter': question_filter, 'pagesize': 100, 'page': page})
+            response, has_even_more, backoff = apiManager.request("/questions", {'filter': question_filter, 'pagesize': 100, 'page': page})
+            has_more = has_even_more
+
+            for item in response["items"]:
+                if "closed_date" not in item or ("closed_date" in item and item["closed_date"] is None):
+                    questions.append({'title': item["title"], 'body': item["body"], 'tags': item["tags"], 'id': item["question_id"]})
+
+            if backoff > 0:
+                print("Back-off received, waiting {0} seconds before repeating request.".format(backoff))
+                time.sleep(backoff)
+
+            page += 1
         except APIException as ex:
             print("[API] [ERROR] Could not fetch tags: #{0} '{1}' - {2}".format(ex.id, ex.name, ex.message))
             return None
-
-        has_more = has_even_more
-
-        for item in response["items"]:
-            if "closed_date" not in item or ("closed_date" in item and item["closed_date"] is None):
-                questions.append({'title': item["title"], 'body': item["body"], 'tags': item["tags"], 'id': item["question_id"]})
-
-        if backoff > 0:
-            print("Back-off received, waiting {0} seconds before repeating request.".format(backoff))
-            time.sleep(backoff)
-
-        page += 1
 
     return questions
 
@@ -197,17 +189,15 @@ def get_related_tags(tags):
     tag_filter = ".IrqzSiB8kWl"
     tag_string = ";".join(tags)
 
-    response, has_more, backoff = None
     try:
-        apiManager.request("/tags/{0}/related".format(tag_string), {'filter': tag_filter})
+        response, has_more, backoff = apiManager.request("/tags/{0}/related".format(tag_string), {'filter': tag_filter})
+        tags = []
+        for item in response["items"]:
+            tags.append(item["name"].replace("-", " "))
+        return tags
     except APIException as ex:
         print("[API] [ERROR] Could not fetch tags: #{0} '{1}' - {2}".format(ex.id, ex.name, ex.message))
         return None
-
-    tags = []
-    for item in response["items"]:
-        tags.append(item["name"].replace("-", " "))
-    return tags
 
 def parse_tags_response(response):
     """
