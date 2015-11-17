0143df99822ee89bba14bb1a442748557fdf60a0
ArtOfCode- <owen.jenkins2903@gmail.com>
2015-11-17 21:01:12 +0000

    algorithm 28B see https://github.com/ArtOfCode-/TagSuggestions/issues?q=milestone%3A%22Alg.+28B%22+is%3Aclosed for a change summary

--- TagSuggestions/main.py
+++ TagSuggestions/main.py
@@ -314,7 +314,7 @@ def parse_tags_response(response):
         tagNames.append(item["name"].replace("-", " "))
 
 
-# Algorithm revision 2015.11.16.27B
+# Algorithm revision 2015.11.17.28B
 def suggest_tags(title, body, tags, related_tags=None, verbose=False):
     """
     Suggests tags for a question, based on its body and current tags.
